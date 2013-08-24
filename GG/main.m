ab5d5920592198b6ad07a568f3bf929dd12fe452
bagiro44 <dmitriyremezov@MacBook-Air-Dmitriy.local>
2013-08-24 19:34:49 +0400

    Initial Commit

---
+++ GG/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GG
+//
+//  Created by Dmitriy Remezov on 24.08.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
